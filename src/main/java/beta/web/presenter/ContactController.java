e54836ff2eecd2b7d2f52238742bd6c493bd14d4
Celine Glißmann <celine.glissmann@gg-net.de>
2019-02-04 15:31:08 +0100

    ContactController und Pom angepasst

--- src/main/java/beta/web/presenter/ContactController.java
+++ src/main/java/beta/web/presenter/ContactController.java
@@ -4,27 +4,26 @@
  * and open the template in the editor.
  */
 package beta.web.presenter;
-
-
 import java.io.Serializable;
-
+import beta.server.eao.ContactEao;
+import beta.server.entity.Contact;
+import java.util.List;
 import javax.annotation.PostConstruct;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import javax.inject.Inject;
+import lombok.Getter;
 /**
  * Controller for Contract
  *
  * @author jens.papenhagen
  */
 public class ContactController implements Serializable {
-
-    private static final Logger L = LoggerFactory.getLogger(ContactController.class);
-
+@Inject
+    ContactEao contactEao;
+    
+    @Getter private List <Contact> contacts;
+    
     @PostConstruct
-    public void init() {
-
-    }
-
+    private void contactControllerInit (){
+        this.contacts = contactEao.findAll();
+                }
 }
