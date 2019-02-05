2f2181f2d93d53d1713835c528cf7b94a59fccce
Celine Glißmann <celine.glissmann@gg-net.de>
2019-02-05 13:20:51 +0100

    Adresse und Communication

--- src/main/java/beta/server/web/ContactController.java
+++ src/main/java/beta/server/web/ContactController.java
@@ -4,15 +4,18 @@
  * and open the template in the editor.
  */
 package beta.server.web;
+
 import java.io.Serializable;
 import beta.server.eao.ContactEao;
 import beta.server.entity.Contact;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import lombok.Getter;
+
 /**
  * Controller for Contact
  *
@@ -21,13 +24,55 @@ import lombok.Getter;
 @Named
 @ViewScoped
 public class ContactController implements Serializable {
-@Inject
+
+    @Inject
     ContactEao contactEao;
-    
-    @Getter private List <Contact> contacts;
-    
+
+    @Getter
+    private List<Contact> contacts;
+
     @PostConstruct
-    private void contactControllerInit (){
+    private void contactControllerInit() {
         this.contacts = contactEao.findAll();
-                }
+    }
+
+    public String zipCode(Contact contact) {
+        return contact.getAddresses()
+               .stream()
+               .map(add -> add.getZipCode())
+               .collect(Collectors.joining("/"));
+
+    }
+    
+    public String city(Contact contact) {
+        return contact.getAddresses()
+                .stream()
+                .map(add -> add.getCity())
+                .collect(Collectors.joining("/"));
+
+    }
+    
+    public String country(Contact contact) {
+        return contact.getAddresses()
+                .stream()
+                .map(add -> add.getCountry().getCountryName())
+                .collect(Collectors.joining("/"));
+
+    }
+    
+    public String street(Contact contact) {
+        return contact.getAddresses()
+                .stream()
+                .map(add -> add.getStreet())
+                .collect(Collectors.joining("/"));
+
+    }
+    
+    public String communication(Contact contact){
+        return contact.getCommunications()
+                .stream()
+                .map(com -> com.getType()+ " :" + com.getIdentifier())
+                .collect(Collectors.joining("/"));
 }
+}
+
